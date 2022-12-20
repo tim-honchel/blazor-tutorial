0f925f89c46d20dd085ead01fdd41c1f99946d20
Timothy Honchel <thonchel@nexient.com>
2022-12-20 15:26:00 -0500

    added API interface

--- MyBlog.Data/Interfaces/IMyBlogApi.cs
+++ MyBlog.Data/Interfaces/IMyBlogApi.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyBlog.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace MyBlog.Data.Interfaces
 {
-    internal interface IMyBlogApi
+    public interface IMyBlogApi
     {
+        Task<int> GetBlogPostCountAsync();
+        Task<List<BlogPost>> GetBlogPostsAsync(int numberOfPosts, int startIndex);
+        Task<List<Category>> GetCategoriesAsync();
+        Task<List<Tag>> GetTagsAsync();
+
+        Task<BlogPost> GetBlogPostAsync(int id);
+        Task<Category> GetCategoryAsync(int id);
+        Task<Tag> GetTagAsync(int id);
+
+        Task<BlogPost> SaveBlogPostAsync(BlogPost item);
+        Task<Category> SaveCategoryAsync(Category item);
+        Task<Tag> SaveTagAsync(Tag item);
+
+        Task DeleteBlogPostAsync(BlogPost item);
+        Task DeleteCategoryAsync(Category item);
+        Task DeleteTagAsync(Tag item);
     }
 }

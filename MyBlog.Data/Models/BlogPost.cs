dedfe462f40526ed40692768555eb0f80cade0e6
Timothy Honchel <thonchel@nexient.com>
2022-12-20 14:58:22 -0500

    added models

--- MyBlog.Data/Models/BlogPost.cs
+++ MyBlog.Data/Models/BlogPost.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyBlog.Data.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace MyBlog.Data.Models
 {
-    internal class BlogPost
+    public class BlogPost : IMyBlogItem
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime PublishDate { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Tag> Tags { get; set; }
     }
 }
--- MyBlog.Data/Models/Tag.cs
+++ MyBlog.Data/Models/Tag.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyBlog.Data.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace MyBlog.Data.Models
 {
-    internal class Tag
+    public class Tag : IMyBlogItem
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<BlogPost> BlogPosts { get; set; }
     }
 }

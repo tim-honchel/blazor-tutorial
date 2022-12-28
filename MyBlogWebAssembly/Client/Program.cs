a49f86ad149d0ec7cf719776a0b7fa98062d9112
Timothy Honchel <thonchel@nexient.com>
2022-12-28 14:46:18 -0500

    added role to database

--- MyBlogWebAssembly/Client/Program.cs
+++ MyBlogWebAssembly/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyBlogWebAssembly.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using MyBlogWebAssembly.Client.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("Authenticated", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)).AddHttpMessageHandler < BaseAddressAuthorizationMessageHandler > ();
 builder.Services.AddHttpClient("Public", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-builder.Services.AddApiAuthorization();
+builder.Services.AddApiAuthorization().AddAccountClaimsPrincipalFactory < RoleAccountClaimsPrincipalFactory > ();
 
 await builder.Build().RunAsync();

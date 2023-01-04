c2ae2ad3aeed612129df76ca50a8370cd0691510
Timothy Honchel <thonchel@nexient.com>
2023-01-04 17:28:28 -0500

    finished testing chapter

---
+++ MyBlog.Shared.Tests/Pages/IndexTest.cs
@@ -0,0 +1,21 @@
+﻿using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using MyBlog.Data.Interfaces;
+using NUnit;
+
+namespace MyBlog.Shared.Tests.Pages
+{
+    public class IndexTest : Bunit.TestContext
+    {
+        public IndexTest()
+        {
+            Services.AddScoped<IMyBlogApi, MyBlogApiMock>();
+        }
+        [Test]
+        public void Shows10Blogposts()
+        {
+            var cut = RenderComponent<MyBlog.Shared.Pages.Index>();
+            Assert.AreEqual(10, cut.FindAll("article").Count);
+        }
+    }
+}
